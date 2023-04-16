5a33897628c23e717b0b9731b29ce88eddb55b33
Momfer de Mol <93478412+momferdemol@users.noreply.github.com>
2023-04-16 16:20:26 +0200

    add hours + clean up code

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 
 <pre>
   Title: A countdown app to play with datetime for learning python
-  Author: Momfert de Mol
+  Author: Momfer de Mol
   Status: WIP
   Created: 16-04-2023
 </pre>
 
-==countdown-app==
+# countdown-app
 
 A simple command-line app to play with Pyton datetime.
--- main.py
+++ main.py
@@ -1,15 +1,16 @@
-import datetime
+from datetime import datetime
 
 
-user_input = input("Enter your goal with a deadline separated by a colon\n")
+user_input = input("Enter your goal with a deadline separated by a colon.\n")
 input_list = user_input.split(":")
 
 goal = input_list[0]
 deadline = input_list[1]
 
-deadline_date = datetime.datetime.strptime(deadline, "%d-%m-%Y")
-today_date = datetime.datetime.today()
+deadline_date = datetime.strptime(deadline, "%d-%m-%Y")
+today_date = datetime.today()
 time_till = deadline_date - today_date
 
-print(f"hi there! Time remaining for your goal: {goal} is {time_till.days}")
+hours_till = int(time_till.total_seconds() / 60 / 60)
+print(f"hi there! Time remaining for your goal: {goal} is {time_till.days} days, or {hours_till} hours.")
 
