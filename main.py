1a848f1c1b99dc38b3c760fdad004487aa5773a1
Momfer de Mol <93478412+momferdemol@users.noreply.github.com>
2023-04-18 19:48:25 +0200

    add ascii art

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import helper
 import functions
+from art import tprint
+
+tprint("deadline dude")
 
 user_input = input("Enter your goal with a deadline separated by a colon.\n")
 
@@ -15,4 +18,3 @@ else:
     days_till = functions.calculate_days(deadline)
     hours_till = functions.calculate_hours(deadline)
     print(f"\n hi there! Time remaining for your goal: {goal} is {days_till.days} days, or {hours_till} hours.\n")
-
